d555cfd7ed6ae03467394d05ec446c338dd2abb5
Igor <Igor-87@mail.ru>
2023-04-14 14:10:31 +0400

    fixed bugs

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -6,20 +6,16 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
     state = {
         char: {
             name: null,
             description: null,
             thumbnail: null,
             homepage: null,
-            wiki: null
+            wiki: null,
         },
         loading: true,
-        error: false
+        error: false,
     }
 
     // Для работы с классом нужно создать его экземпляр
@@ -64,11 +60,37 @@ class RandomChar extends Component {
             .catch(this.onError); // ошибка при получении данных
     }
 
+    // Хук - компонент появляется на странице (монтируется)
+    componentDidMount() {
+        this.updateChar();
+        // this.intervalId = setInterval(this.updateChar, 3000);
+    }
+
+    // Хук - компонент удаляется со страницы
+    componentWillUnmount() {
+        // clearInterval(this.intervalId);
+    }
+
+    // Обновление персонажа при нажатии кнопки try it
+    onBtnUpdate = () => {
+        this.setState(
+            {
+                loading: true,
+                error: false
+            })
+        this.updateChar();
+    }
+
+    resizeImg = (img) => {
+        console.log(img)
+    }
+
+
     render() {
         const { char, loading, error } = this.state;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(error || loading) ? <View char={char}/> : null; // если нет ошибки или если нет загрузки то возращаем контент
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(error || loading) ? <View char={char} /> : null; // если нет ошибки или если нет загрузки то возращаем контент
 
         return (
             <div className="randomchar" >
@@ -84,7 +106,7 @@ class RandomChar extends Component {
                         Or choose another one
                     </p>
                     <button className="button button__main">
-                        <div className="inner">try it</div>
+                        <div onClick={this.onBtnUpdate} className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
                 </div>
@@ -95,10 +117,14 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
     const { name, description, thumbnail, homepage, wiki } = char
-
+    let notImage = false;
+    if (char.thumbnail.indexOf('available') > -1) {
+        notImage = true
+    }
+    const style = notImage ? "contain" : '';
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <img src={thumbnail} style={{objectFit: style}} alt="Random character" className="randomchar__img" />
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">

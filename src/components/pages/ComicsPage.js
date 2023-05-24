39ecaaab766297a943d077a19fc31b165d712e7a
Igor <Igor-87@mail.ru>
2023-05-24 12:59:03 +0400

    using Helmet for SEO optimization

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,18 @@
+import { Helmet } from "react-helmet";
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
+
 
 const ComicsPage = () => {
     return (
         <>
+        <Helmet>  {/* сторонняя бибиотека, позволяет менять title и др на страницах для SEO оптимизации */}
+                <meta
+                    name="description"
+                    content="Page with list of our comics"
+                />
+                <title>Comics Page</title>
+            </Helmet>
             <AppBanner></AppBanner>
             <ComicsList />
         </>
